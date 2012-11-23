5856f588d627b2d86a20cd00a007c2ca6894d922
James Thorne <james@dtscc.net>
2012-11-23 15:33:41 -0700

    Added vector

--- course-project/misc/Vector.cxx
+++ course-project/misc/Vector.cxx
@@ -1,52 +1,37 @@
-#include <QtGlobal>
-
 #include <Vector.h>
+#include <Quaternion.h>
 #include <cmath>
 
 Vector::Vector()
-   : x_(0.0)
-   , y_(0.0)
-   , z_(0.0)
+   : x(0.0)
+   , y(0.0)
+   , z(0.0)
 {
 }
 
-Vector::Vector(double x_, double y_, double z_)
-   : x_(x_)
-   , y_(y_)
-   , z_(z_)
+Vector::Vector(double x, double y, double z)
+   : x(x)
+   , y(y)
+   , z(z)
 {
 }
 
-Vector::Vector(float coords[])
-   : x_(coords[0])
-   , y_(coords[1])
-   , z_(coords[2])
+Vector::Vector(const Quaternion& quaternion)
+   : x(quaternion.x)
+   , y(quaternion.y)
+   , z(quaternion.z)
 {
 }
 
-/**
- ******************************************************************************
- *
- *                   Add another vector to this one
- *
- ******************************************************************************
- */
 Vector& Vector::operator += (const Vector& rhs)
 {
-   x_ += rhs.x_;
-   y_ += rhs.y_;
-   z_ += rhs.z_;
+   x += rhs.x;
+   y += rhs.y;
+   z += rhs.z;
 
    return *this;
 }
 
-/**
- ******************************************************************************
- *
- *                   Add two vectors
- *
- ******************************************************************************
- */
 Vector Vector::operator + (const Vector& rhs) const
 {
    Vector result = *this;
@@ -55,159 +40,89 @@ Vector Vector::operator + (const Vector& rhs) const
    return result;
 }
 
-/**
- ******************************************************************************
- *
- *                   Subtract two vectors
- *
- ******************************************************************************
- */
 Vector Vector::operator - (const Vector& rhs) const
 {
-   return Vector(x_ - rhs.x_, y_ - rhs.y_, z_ - rhs.z_);
+   return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
 }
 
-/**
- ******************************************************************************
- *
- *                   Return the largest individual element
- *
- ******************************************************************************
- */
-double Vector::largestElement() const
-{
-   return qMax(qMax(x_, y_), z_);
-}
-
-/**
- ******************************************************************************
- *
- *                   Cross product
- *
- ******************************************************************************
- */
 Vector Vector::cross(const Vector& rhs) const
 {
    const Vector& one = *this;
    const Vector& two = rhs;
 
-   return Vector( (one.y_ * two.z_) - (two.y_ * one.z_),
-                  (one.z_ * two.x_) - (two.z_ * one.x_),
-                  (one.x_ * two.y_) - (two.x_ * one.y_) );
+   return Vector( (one.y * two.z) - (two.y * one.z),
+                  (one.z * two.x) - (two.z * one.x),
+                  (one.x * two.y) - (two.x * one.y) );
 }
 
-/**
- ******************************************************************************
- *
- *                   Dot product
- *
- ******************************************************************************
- */
 double Vector::dot(const Vector& rhs) const
 {
-   return (x_*rhs.x_) + (y_*rhs.y_) + (z_*rhs.z_);
+   return (x*rhs.x) + (y*rhs.y) + (z*rhs.z);
 }
 
-/**
- ******************************************************************************
- *
- *                   Return vector magnitude
- *
- ******************************************************************************
- */
+Vector Vector::rotate(const Quaternion& angle) const
+{
+   return Vector(angle * Quaternion(*this) * angle.inverse());
+}
+
 double Vector::magnitude() const
 {
-   return sqrt(x_*x_ + y_*y_ + z_*z_);
+   return sqrt(x*x + y*y + z*z);
 }
 
-/**
- ******************************************************************************
- *
- *                   Return a normalized vector (i.e. magnitude = 1)
- *
- ******************************************************************************
- */
 Vector Vector::normalized() const
 {
-   double scale = magnitude();
-   double x = x_ / scale;
-   double y = y_ / scale;
-   double z = z_ / scale;
-
-   return Vector(x, y, z);
+   Vector result = *this;
+   result.normalize();
+   return result;
 }
 
-/**
- ******************************************************************************
- *
- *                   Multiply by a scalar
- *
- ******************************************************************************
- */
+void Vector::normalize()
+{
+   double scale = magnitude();
+   x /= scale;
+   y /= scale;
+   z /= scale;
+}
+
 Vector operator*(const Vector& vector, double scalar)
 {
-   return Vector(vector.x() * scalar, vector.y() * scalar, vector.z() * scalar);
-}
-Vector operator*(double scalar, const Vector& vector)
-{
-   return Vector(vector.x() * scalar, vector.y() * scalar, vector.z() * scalar);
+   return Vector(vector.x * scalar, vector.y * scalar, vector.z * scalar);
 }
 
-/**
- ******************************************************************************
- *
- *                   Divide by a scalar
- *
- ******************************************************************************
- */
-Vector operator/(const Vector& vector, double scalar)
-{
-   return Vector(vector.x() / scalar, vector.y() / scalar, vector.z() / scalar);
-}
-
-/**
- ******************************************************************************
- *
- *                   Return a string representation (for debugging)
- *
- ******************************************************************************
- */
 QString Vector::toString() const
 {
-   return QString::number(x_, 'f', 6) + " " + QString::number(y_, 'f', 6) + " " + QString::number(z_, 'f', 6);
+   return QString::number(x, 'f', 6) + " " + QString::number(y, 'f', 6) + " " + QString::number(z, 'f', 6);
 }
 
-/**
- ******************************************************************************
- *
- *                   Calculate the inverse of this vector
- *
- ******************************************************************************
- */
 Vector Vector::inverse() const
 {
-   return Vector(1.0/x_, 1.0/y_, 1.0/z_);
+   return Vector(1.0/x, 1.0/y, 1.0/z);
 }
 
-/**
- ******************************************************************************
- *
- *                   Multiply element-wise
- *
- ******************************************************************************
- */
 Vector Vector::multiplyElementsBy(const Vector& rhs) const
 {
-   return Vector(x_*rhs.x_, y_*rhs.y_, z_*rhs.z_);
+   return Vector(x*rhs.x, y*rhs.y, z*rhs.z);
 }
 
-/**
- ******************************************************************************
- *
- *                   Bound to a maximum magnitude
- *
- ******************************************************************************
- */
+Quaternion Vector::rotationTo(const Vector& lookAt) const
+{
+   Vector axis = this->cross(lookAt);
+   axis.normalize();
+
+   double angle = acos(this->dot(lookAt));
+
+   if (angle < 0.000001)
+   {
+      return Quaternion();
+   }
+
+   Quaternion result = Quaternion(angle, axis);
+   result.normalize();
+
+   return result;
+}
+
 Vector Vector::boundedToMagnitude(double maxMagnitude) const
 {
    double currentMagnitude = this->magnitude();
@@ -220,60 +135,46 @@ Vector Vector::boundedToMagnitude(double maxMagnitude) const
    return (*this) * (1.0/scale);
 }
 
-/**
- ******************************************************************************
- *
- *                   Calculate the distance between vectors
- *
- ******************************************************************************
- */
 double Vector::distanceTo(const Vector rhs) const
 {
    return (*this - rhs).magnitude();
 }
 
-/**
- ******************************************************************************
- *
- *                   Interpolate two vectors by a scaling factor
- *
- ******************************************************************************
- */
+const Vector Vector::intersectionBetweenLineAndSphere(Vector lineStart, Vector direction,
+                                                     Vector sphereCenter, double sphereRadius)
+{
+   // Math is hard.
+   // Lazy is easier.
+
+   double originalDistance = lineStart.distanceTo(sphereCenter) - sphereRadius;
+   double testDistance = 0.0;
+   double delta = 0.1;
+
+   direction.normalize();
+
+   while (true)
+   {
+      Vector testPoint = lineStart + (direction * testDistance);
+
+      double newDistance = testPoint.distanceTo(sphereCenter) - sphereRadius;
+      if ( newDistance < 0 )
+      {
+         return testPoint;
+      }
+
+      if (newDistance > originalDistance)
+      {
+         return lineStart;
+      }
+
+      testDistance += delta;
+   }
+ 
+   return lineStart;  
+}
+
 const Vector Vector::interpolate(Vector v1, Vector v2, double t)
 {
    return (v1 * t) + (v2 * (1.0 - t));
 }
 
-/**
- ******************************************************************************
- *
- *                   Accessors
- *
- ******************************************************************************
- */
-double Vector::x() const
-{
-   return x_;
-}
-
-double Vector::y() const
-{
-   return y_;
-}
-
-double Vector::z() const
-{
-   return z_;
-}
-
-/**
- ******************************************************************************
- *
- *                   Reflection
- *
- ******************************************************************************
- */
-Vector Vector::reflected(const Vector normal) const
-{
-   return *this - ((2 * this->dot(normal)) * normal);
-}
